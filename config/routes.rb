94d2018a9f28eaa7e05c92f53444b857966722a2
Javier Perez <perez.javi.a@gmail.com>
2014-09-20 16:48:59 -0400

    deleted user scaffold and moved partials to views/layouts

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :users
   resources "contacts", only: [:new, :create]
 
 
@@ -7,7 +6,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'users#index'
+  root 'contacts#new'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
