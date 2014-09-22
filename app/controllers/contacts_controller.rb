e83bcc65ac4c177a29708b596dba5be99f40fe2e
Javier Perez <perez.javi.a@gmail.com>
2014-09-22 13:09:37 -0400

    made changes and added links to whyme and about sec, also added error for contact form

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -8,9 +8,9 @@ class ContactsController < ApplicationController
 		@contact.request = request
 		if @contact.deliver
 			flash.now[:error] = nil
-			flash.now[:notice] = "Thank you for your message. I will get back to you as soon as I can."
+			flash.now[:notice] = "Thank you for your message. I will get back to you soon."
 		else
-			flash.now[:error] = "Cannot send message."
+			flash.now[:error] = "We could not send your message. Please try again!"
 			render :new
 		end
 	end
